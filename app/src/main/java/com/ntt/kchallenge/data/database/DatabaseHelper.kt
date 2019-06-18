3aaf6c4d022d8a6d21ce0cc731986635a125a398
Thuat Nguyen <thuat.nguyen@honestbee.com>
2019-06-19 00:41:47 +0800

    Update user model

---
+++ app/src/main/java/com/ntt/kchallenge/data/database/DatabaseHelper.kt
@@ -0,0 +1,2 @@
+package com.ntt.kchallenge.data.db
+
--- app/src/main/java/com/ntt/kchallenge/data/model/User.kt
+++ app/src/main/java/com/ntt/kchallenge/data/model/User.kt
@@ -3,7 +3,8 @@ package com.ntt.kchallenge.data.model
 /**
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
-data class LoggedInUser(
+data class User(
     val userId: String,
-    val displayName: String
+    val username: String,
+    val password: String
 )
